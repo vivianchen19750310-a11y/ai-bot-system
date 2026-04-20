a517fb6a49661cf822dcb0a25ae9cf3f886f966c
vivianchen19750310-a11y <vivianchen19750310@gmail.com>
2026-04-21 00:00:55 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,13 @@ print("bot started")
 
 def bot_reply(message):
     if "懶人包" in message:
-        print("懶人包已記錄")
+        print("分類：懶人包")
     elif "團購" in message:
-        print("團購已記錄")
+        print("分類：團購")
     elif "保健" in message:
-        print("保健已記錄")
+        print("分類：保健保養")
     else:
-        print("其他已記錄")
+        print("分類：其他")
 
+# 模擬一則留言
 bot_reply("懶人包")
