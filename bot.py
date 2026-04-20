8994e7c7ea24c213da6c52e9ee9faa79fbb765d8
vivianchen19750310-a11y <vivianchen19750310@gmail.com>
2026-04-20 23:23:33 +0800

    Refactor bot_reply to log messages instead of reply
    Refactored bot_reply function to log specific keywords and removed previous return statements.

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
-def bot_reply(message):
-    if any(word in message for word in ["懶人包", "清單", "資訊", "給我", "有嗎"]):
-        return "我幫你整理好懶人包了，這邊可以直接看👇"
-    elif any(word in message for word in ["團購", "我要", "哪裡買", "下單"]):
-        return "目前有團購優惠，我幫你整理好連結👇"
-    elif any(word in message for word in ["保健", "保養", "怎麼選", "適合我嗎", "推薦", "吃什麼", "擦什麼"]):
-        return "我可以幫你評估適合的保健或保養產品，請問你的需求或困擾是什麼呢？"
-    else:
-        return "我收到你的訊息了，我幫你整理後回覆你"
+print("bot started")
 
-user_message = input("請輸入訊息：")
-print(bot_reply(user_message))
+def bot_reply(message):
+    if "懶人包" in message:
+        print("懶人包已記錄")
+    elif "團購" in message:
+        print("團購已記錄")
+    elif "保健" in message:
+        print("保健已記錄")
+    else:
+        print("其他已記錄")
+
+bot_reply("懶人包")
